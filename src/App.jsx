91e84616b2182fef66bcac093cd6820329826293
Agustin Macoggi <agustinmacoggi@gmail.com>
2024-02-02 18:27:40 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
 	return (
-		<h1 className='text-3xl font-bold underline text-red-500'>Hello world!</h1>
+		<h1 className='text-3xl font-bold underline text-red-500'>Hello world! xd</h1>
 	)
 }
 
