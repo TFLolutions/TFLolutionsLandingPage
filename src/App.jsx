71cd66c80b8a88c40cd3fca2d3c7fc8e9e435562
Nahuel <Nahuelpardo3@gmail.com>
2024-02-29 20:08:55 -0300

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,14 @@ import NavBar from './components/NavBar'
 
 function App() {
 	return (
-<<<<<<< HEAD
 		<>
+			<NavBar/>
 			<Inicio />
 			<Servicios />
 			<Nosotros/>
 			<ContactoStepper />
 			<Footer/>
 		</>
-=======
-	  <>
-	  	<NavBar/>
-		<div id="inicio">
-		  <Inicio />
-		</div>
-		<div id="servicios">
-		  <Servicios />
-		</div>
-		<div id="contacto">
-		  <ContactoStepper />
-		</div>
-		<div id="nosotros">
-		  <Nosotros/>
-		</div>
-		<Footer/>
-	  </>
->>>>>>> 39e156bb6339b2fdef08c1149ac1e93813900c72
 	)
   }
   
