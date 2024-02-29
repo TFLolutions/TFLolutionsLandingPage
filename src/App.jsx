50ef7a2ac51551380550b51d8ec9590459dce4f4
Nahuel <Nahuelpardo3@gmail.com>
2024-02-29 20:08:17 -0300

    nashe

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import Inicio from './components/Inicio'
 import Footer from './components/Footer'
 import ContactoStepper from './components/ContactoStepper'
 import Nosotros from './components/Nosotros'
+import NavBar from './components/NavBar'
 
 function App() {
 	return (
+<<<<<<< HEAD
 		<>
 			<Inicio />
 			<Servicios />
@@ -13,7 +15,26 @@ function App() {
 			<ContactoStepper />
 			<Footer/>
 		</>
+=======
+	  <>
+	  	<NavBar/>
+		<div id="inicio">
+		  <Inicio />
+		</div>
+		<div id="servicios">
+		  <Servicios />
+		</div>
+		<div id="contacto">
+		  <ContactoStepper />
+		</div>
+		<div id="nosotros">
+		  <Nosotros/>
+		</div>
+		<Footer/>
+	  </>
+>>>>>>> 39e156bb6339b2fdef08c1149ac1e93813900c72
 	)
-}
-
-export default App
+  }
+  
+  export default App;
+  
