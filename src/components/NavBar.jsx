032c46374323d2e28afdb9b3f9766d60bbe46072
FrancisRkbr <frannedm@gmail.com>
2024-03-06 04:17:09 -0300

    NavBar Rediseñado, Agregado Menu mobile, comentarios en codigo.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,62 +1,93 @@
-import React from "react";
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import Logo from './Logo/Logo.png'
 
+const navlinks = [
+  {
+    title: 'Inicio',
+    link: '#',
+  },
+  {
+    title: 'Servicios',
+    link: '#servicios',
+  },
+  {
+    title: 'Contacto',
+    link: '#contacto',
+  },
+  {
+    title: 'Nosotros',
+    link: '#nosotros',
+  },
+  {
+    title: 'Clientes',
+    link: '#clientes',
+  },
+];
+
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleMenu = () => {
+    setOpen((prev) => !prev);
+  };
+
   return (
-    <nav className="flex justify-between items-center bg-white dark:bg-gray-900 sticky top-0 scroll-py-1">
-      <div className="flex items-center">
-        <a href="#"className="flex items-center">
-        <img
-          src={Logo}
-          alt="Logo"
-          className="w-20 h-auto mr-1"
-        />
-        <span className="text-xl font-semibold text-white">No Me Pagan</span>
-        </a>
+    <div className='bg-black relative top-0 z-50'>
+      <div className='mx-auto px-4 sm:px-6 lg:px-8'>
+        <div className='flex items-center justify-between h-16'>
+          <div className='flex items-center justify-center'>
+            <a href='/' className='text-white font-semibold'>
+              <img src={Logo} alt='Logo' className='h-8 ml-6 '/>{/*LOGO PERRIX*/}
+              No me Pagan
+            </a>
+          </div>
+          {/*NAVLINKS*/}
+          <div className='-mr-2 flex-grow md:flex md:items-center md:justify-center'>
+            <div className='hidden md:block'>
+              <div className='nl-10 flex items-baseline space-x-4'>
+                {navlinks.map((link, index) => (
+                  <a
+                    key={index}
+                    className='text-gray-300 transition-all duration-500 hover:text-blue-600 px-3 py-2 rounded-md text-md font-semibold'
+                    href={link.link}
+                  >
+                    {link.title}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+          {/*Burgir-button*/}
+          <div className='-mr-2 flex md:hidden'>
+            <button
+              type='button'
+              onClick={handleMenu}
+              className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'
+            >
+              <span className='sr-only'>Open Main Menu</span>
+              {open ? <FaTimes /> : <FaBars />}
+            </button>
+          </div>
+        </div>
       </div>
-      <ul className="flex justify-end flex-1 mr-10">
-        <li>
-          <a
-            href="#inicio"
-            className="block text-white px-2 py-1 rounded hover:bg-black hover:duration-500 "
-          >
-            Inicio  
-          </a>
-        </li>
-        <span className="text-white mx-2 mt-1 mb-1">|</span>
-        <li>
-          <a
-            href="#servicios"
-            className="block text-white px-2 py-1 rounded hover:bg-black hover:duration-500"
-          >
-            Servicios 
-          </a>
-        </li>
-        <span className="text-white mx-2 mt-1 mb-1">|</span>
-        <li>
-        <a
-            href="#nosotros"
-            className="block text-white px-2 py-1 rounded hover:bg-black hover:duration-500"
-          >
-            Nosotros 
-          </a>
-          </li>
-          <span className="text-white mx-2 mt-1 mb-1">|</span>
-          <li>
-          <a
-            href="#clientes"
-            className="block text-white px-2 py-1 rounded hover:bg-black hover:duration-500"
-          >
-            Clientes 
-          </a>
-          </li>
-      </ul>
-      <div className="menu flex flex-col justify-between items-center">
-        <span className="h-1 w-full bg-white rounded mb-1"></span>
-        <span className="h-1 w-full bg-white rounded mb-1"></span>
-        <span className="h-1 w-full bg-white rounded mb-1"></span>
+      {/*Menu Mobile*/}
+      <div className={`md:hidden transition-all duration-500 ease-in-out ${open ? 'block' : 'hidden'}`}>
+        <div className='ox-2 pt-2 pb-3 space-y-1 sm:px-3'>
+          <div className='flex flex-col items-center'>
+            {navlinks.map((link, index) => (
+              <a
+                key={index}
+                className='text-gray-300 hover:text-blue-600 transition duration-500 hover:scale-110 hover:-translate-y-1 ease-in-out block px-3 py-2 rounded-md text-base font-medium'
+                href={link.link}
+              >
+                {link.title}
+              </a>
+            ))}
+          </div>
+        </div>
       </div>
-    </nav>
+    </div>
   );
 };
 
