66925da75f8dff9389bc27961c90aa8a51ad5ebe
FrancisRkbr <frannedm@gmail.com>
2024-03-07 03:05:59 -0300

    Agregado Button to Top segun el Figma

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
-import Logo from './Logo/Logo.png'
+import Logo from './Logo/Logo.png';
+import uparrow from '../../public/Servicios/uparrow.svg'
 
 const navlinks = [
   {
@@ -27,10 +28,33 @@ const navlinks = [
 
 const NavBar = () => {
   const [open, setOpen] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
   const handleMenu = () => {
     setOpen((prev) => !prev);
   };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  const handleScroll = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <div className='bg-black relative top-0 z-50'>
@@ -38,7 +62,7 @@ const NavBar = () => {
         <div className='flex items-center justify-between h-16'>
           <div className='flex items-center justify-center'>
             <a href='/' className='text-white font-semibold'>
-              <img src={Logo} alt='Logo' className='h-8 ml-6 '/>{/*LOGO PERRIX*/}
+              <img src={Logo} alt='Logo' className='h-8 ml-6' />
               No me Pagan
             </a>
           </div>
@@ -63,7 +87,8 @@ const NavBar = () => {
             <button
               type='button'
               onClick={handleMenu}
-              className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'
+              className='inline-flex items-center justify-center p-2 rounded-md
+               text-gray-400 hover:text-white hover:bg-gray-700 animate-pulse'
             >
               <span className='sr-only'>Open Main Menu</span>
               {open ? <FaTimes /> : <FaBars />}
@@ -78,7 +103,8 @@ const NavBar = () => {
             {navlinks.map((link, index) => (
               <a
                 key={index}
-                className='text-gray-300 hover:text-blue-600 transition duration-500 hover:scale-110 hover:-translate-y-1 ease-in-out block px-3 py-2 rounded-md text-base font-medium'
+                className='text-gray-300 hover:text-blue-600 transition duration-500 hover:scale-110 hover:-translate-y-1 ease-in-out
+                block px-3 py-2 rounded-md text-base font-medium'
                 href={link.link}
               >
                 {link.title}
@@ -87,6 +113,16 @@ const NavBar = () => {
           </div>
         </div>
       </div>
+      {/* Button to top */}
+      {isVisible && (
+        <button
+          className='fixed bottom-6 right-6 bg-gray-500 hover:bg-gray-700 transition-all duration-500 text-white font-bold 
+          py-3.5 px-3.5 rounded-full animate-bounce'
+          onClick={scrollToTop}
+        >
+          <img src={uparrow} alt='To top' className='h-5 w-5' />
+        </button>
+      )}
     </div>
   );
 };
