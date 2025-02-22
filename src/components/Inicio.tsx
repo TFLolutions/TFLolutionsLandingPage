bb7a6d4a4ad6e1fbd7b2fa33fdccfba29f75f3d6
Franco vazquez <fraan_v1@hotmail.com>
2025-02-22 17:59:05 -0300

    Deploy for netlify

--- src/components/Inicio.tsx
+++ src/components/Inicio.tsx
@@ -24,7 +24,7 @@ const Inicio: FC = () => {
                     className="w-24 h-24 object-contain"
                 />
                 <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-                    MOTHERSHIP SOFTWARE SOLUTIONS.
+                    .MOTHERSHIP SOFTWARE SOLUTIONS.
                 </h1>
                 <img 
                     src={logo} 
